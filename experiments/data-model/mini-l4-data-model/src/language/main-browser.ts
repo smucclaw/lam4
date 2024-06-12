23607a1fff35c4f0ba2f8bd8e7750b2d0086105d
ymh <hanyongming@gmail.com>
2024-06-12 20:20:51 +0800

    Add initial draft of grammar for data model (see the .langium file for the grammar)

---
+++ experiments/data-model/mini-l4-data-model/src/language/main-browser.ts
@@ -0,0 +1,15 @@
+import { EmptyFileSystem } from 'langium';
+import { startLanguageServer } from 'langium/lsp';
+import { BrowserMessageReader, BrowserMessageWriter, createConnection } from 'vscode-languageserver/browser.js';
+import { createMiniL4TypesServices } from './mini-l4-types-module.js';
+
+declare const self: DedicatedWorkerGlobalScope;
+
+const messageReader = new BrowserMessageReader(self);
+const messageWriter = new BrowserMessageWriter(self);
+
+const connection = createConnection(messageReader, messageWriter);
+
+const { shared } = createMiniL4TypesServices({ connection, ...EmptyFileSystem });
+
+startLanguageServer(shared);
