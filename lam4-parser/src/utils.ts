b551438f793960a090c999debf281ff31885ffbb
ymh <hanyongming@gmail.com>
2024-06-24 18:31:19 +0800

    Add utils for branded types

---
+++ lam4-parser/src/utils.ts
@@ -0,0 +1,7 @@
+/** Branded types 
+ * https://egghead.io/blog/using-branded-types-in-typescript
+ *  See also the TS playground excerpt linked from the article above
+ */
+declare const __brand: unique symbol;
+type Brand<B> = { [__brand]: B };
+export type Branded<T, B> = T & Brand<B>;
