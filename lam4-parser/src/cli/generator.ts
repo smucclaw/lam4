d94453024f3a116dea37d23ea2fedf08ed5b72dc
ymh <hanyongming@gmail.com>
2024-06-24 19:11:35 +0800

    Add functionality for serializing AST to JSON (in a way that won't break even if the AST / parse rules change)

--- lam4-parser/src/cli/generator.ts
+++ lam4-parser/src/cli/generator.ts
@@ -1,8 +1,37 @@
+import { JSONString } from '../utils.js';
 import type { Program } from '../language/generated/ast.js';
 // import { expandToNode, joinToNode, toString } from 'langium/generate';
+import chalk from 'chalk';
 import * as fs from 'node:fs';
 import * as path from 'node:path';
 import { extractDestinationAndName } from './cli-util.js';
+import { createLam4Services } from '../language/lam4-module.js';
+import { NodeFileSystem } from 'langium/node';
+
+export function generateAST(program: Program): JSONString {
+    const services = createLam4Services(NodeFileSystem).lam4Services;
+    
+    const astJson = services.serializer.JsonSerializer.serialize(program);
+
+    // for quick debugging
+    const parsedJson = JSON.parse(astJson);
+    console.log(chalk.cyan(JSON.stringify(parsedJson, null, 2)));
+    
+    return astJson as JSONString;
+}
+
+export function generateAndSaveAST(program: Program, filePath: string, destination: string | undefined): string {
+    const data = extractDestinationAndName(filePath, destination);
+    const outFilePath = `${path.join(data.destination, data.name)}.json`;
+
+    const astJson = generateAST(program);
+
+    if (!fs.existsSync(data.destination)) {
+        fs.mkdirSync(data.destination, { recursive: true });
+    }
+    fs.writeFileSync(outFilePath, astJson);
+    return outFilePath;
+}
 
 export function generateJavaScript(program: Program, filePath: string, destination: string | undefined): string {
     const data = extractDestinationAndName(filePath, destination);
--- lam4-parser/src/cli/main.ts
+++ lam4-parser/src/cli/main.ts
@@ -4,7 +4,9 @@ import { Command } from 'commander';
 import { Lam4LanguageMetaData } from '../language/generated/module.js';
 import { createLam4Services } from '../language/lam4-module.js';
 import { extractAstNode } from './cli-util.js';
-import { generateJavaScript } from './generator.js';
+import { 
+    // generateJavaScript, 
+    generateAndSaveAST } from './generator.js';
 import { NodeFileSystem } from 'langium/node';
 import * as url from 'node:url';
 import * as fs from 'node:fs/promises';
@@ -14,11 +16,14 @@ const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 const packagePath = path.resolve(__dirname, '..', '..', 'package.json');
 const packageContent = await fs.readFile(packagePath, 'utf-8');
 
-export const generateAction = async (fileName: string, opts: GenerateOptions): Promise<void> => {
-    const services = createLam4Services(NodeFileSystem).Lam4;
+export const toAstAction = async (fileName: string, opts: GenerateOptions): Promise<void> => {
+    const services = createLam4Services(NodeFileSystem).lam4Services;
     const program = await extractAstNode<Program>(fileName, services);
-    const generatedFilePath = generateJavaScript(program, fileName, opts.destination);
-    console.log(chalk.green(`JavaScript code generated successfully: ${generatedFilePath}`));
+    // const generatedFilePath = generateJavaScript(program, fileName, opts.destination);
+    // console.log(chalk.green(`JavaScript code generated successfully: ${generatedFilePath}`));
+
+    const astJsonOutPath = generateAndSaveAST(program, fileName, opts.destination);
+    console.log(chalk.green(`AST serialized to JSON at ${astJsonOutPath}`));
 };
 
 export type GenerateOptions = {
@@ -32,11 +37,11 @@ export default function(): void {
 
     const fileExtensions = Lam4LanguageMetaData.fileExtensions.join(', ');
     program
-        .command('generate')
+        .command('toAst')
         .argument('<file>', `source file (possible file extensions: ${fileExtensions})`)
         .option('-d, --destination <dir>', 'destination directory of generating')
-        .description('generates JavaScript code that prints "Hello, {name}!" for each greeting in a source file')
-        .action(generateAction);
+        .description('serialize AST to JSON')
+        .action(toAstAction);
 
     program.parse(process.argv);
 }
