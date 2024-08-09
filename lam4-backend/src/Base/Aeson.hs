1bf860e74076414cb2cb99c72c82954c7682bd2f
ymh <hanyongming@gmail.com>
2024-08-10 01:19:09 +0800

    update deps (remove `aeson value parser`; add `aeson-combinators`)

--- lam4-backend/src/Base/Aeson.hs
+++ lam4-backend/src/Base/Aeson.hs
@@ -1,10 +1,7 @@
-module Base.Aeson (module X, fromJSONValue) where
+module Base.Aeson (module X) where
 
-import Data.Aeson as X
+-- import Data.Aeson as X
 import Data.Aeson.Types as X
 import Data.Aeson.KeyMap as X
 import Data.Aeson.Key as X
 import Data.Aeson.Optics as X
-
-fromJSONValue :: FromJSON a => Value -> Maybe a
-fromJSONValue = parseMaybe parseJSON
