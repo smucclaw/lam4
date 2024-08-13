b05f2aa18bda0169d5760e537bcefb2d1d89e60d
ymh <hanyongming@gmail.com>
2024-08-13 15:21:29 +0800

    deps, base

--- lam4-backend/src/Base.hs
+++ lam4-backend/src/Base.hs
@@ -1,27 +1,25 @@
--- | Defines a project-specific "prelude". Adapted from Simala, though I think this is a pretty common practice
+-- | Defines a project-specific "prelude". Adapted from Simala, though I think this is a common practice
 --
 module Base (module X) where
 
-import Control.Monad as X
-import Control.Monad.Except as X
-import Control.Monad.Identity as X
-import Control.Monad.Reader as X
-import Control.Monad.State as X
-import Data.Coerce as X
+import           Control.Monad          as X
+import           Control.Monad.Except   as X
+import           Control.Monad.Identity as X
+import           Control.Monad.Reader   as X
+import           Control.Monad.State    as X
+import           Data.Coerce            as X
 -- import Data.IORef as X
-import Data.Kind as X
-import Data.List as X hiding (uncons)
-import Data.Map.Strict as X (Map, (!))
-import Data.Maybe as X
-import Data.Set as X (Set)
-import Data.String as X
-import Data.Text as X (Text)
-import Data.Void as X
-import GHC.Generics as X (Generic)
-import Optics.Setter as X
-import Optics.State as X
-import Optics.State.Operators as X ((.=), (%=), (<%=))
-import Optics.At as X
-import Optics as X
-import Prettyprinter as X (Doc, Pretty(..), (<+>))
-import System.IO as X
+import           Data.Kind              as X
+import           Data.List              as X hiding (uncons)
+import           Data.List.NonEmpty     as X (NonEmpty (..))
+import           Data.Map.Strict        as X (Map, (!))
+import           Data.Maybe             as X
+import           Data.Set               as X (Set)
+import           Data.String            as X
+import           Data.Text              as X (Text)
+import           Data.Void              as X
+import           GHC.Generics           as X (Generic)
+import           Optics                 as X
+import           Optics.State.Operators as X ((%=), (.=), (<%=))
+import           Prettyprinter          as X (Doc, Pretty (..), (<+>))
+import           System.IO              as X
