801bbf6e18fb7b746c7bc9278c1bc9d288979e80
ymh <hanyongming@gmail.com>
2024-08-17 03:22:47 +0800

    Main.hs: Add simple driver that currently just parses files and, when successful, prints the concrete syntax

--- lam4-backend/app/Main.hs
+++ lam4-backend/app/Main.hs
@@ -1,7 +1,47 @@
 module Main where
 
--- import qualified MyLib (someFunc)
+import           Base
+import qualified Base.ByteString     as BL hiding (null)
+import           Lam4.Expr.Parser    (parseProgramByteStr)
+import           Lam4.Parser.Monad   (evalParserFromScratch)
+import           Options.Applicative as Options
+
+data Options =
+  MkOptions
+    { files   :: [FilePath]
+    }
+
+optionsDescription :: Options.Parser Options
+optionsDescription =
+  MkOptions
+  <$> many (strArgument (metavar "CONCRETE SYNTAX JSONs..."))
+
+optionsConfig :: Options.ParserInfo Options
+optionsConfig =
+  info (optionsDescription <**> helper)
+    (  fullDesc
+    <> header "Lam4 Backend"
+    )
+
 
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
+  options <- Options.execParser optionsConfig
+  if null options.files
+    then do
+      hPutStrLn stderr "Lam4 Backend: no input files given; use --help for help"
+    else do parseProgramFiles options.files
+
+parseProgramFile :: FilePath -> IO ()
+parseProgramFile file = do
+  bsFile <- BL.readFile file
+  case evalParserFromScratch . parseProgramByteStr $ bsFile of
+    Left err  -> pPrint err
+    Right cst -> pPrint cst
+
+parseProgramFiles :: [FilePath] -> IO ()
+parseProgramFiles inputFiles = do
+  forM_ inputFiles (\f -> do
+    pPrint f
+    parseProgramFile f)
+    
