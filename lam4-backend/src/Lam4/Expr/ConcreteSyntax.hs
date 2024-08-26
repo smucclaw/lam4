96a01853c93c0e051b6f617556419a8fa4daa92e
ymh <hanyongming@gmail.com>
2024-08-26 18:08:36 +0800

    Tweak grammar / concrete syntax

--- lam4-backend/src/Lam4/Expr/ConcreteSyntax.hs
+++ lam4-backend/src/Lam4/Expr/ConcreteSyntax.hs
@@ -56,17 +56,20 @@ data Expr
     What follows is
     EXPERIMENTAL or VERY WIP
   ============================-}
+  | NormIsInfringed Name                           -- NormIsInfringed NameOfNorm.
+                                                   -- Sugar for a predicate checking if @nameOfNorm@ is violated (users can supply unique identifiers for Deontics and IfThenOtherwise statements that contain a Deontic)
 
-  {------------------------------------------------------------
-  Stuff that's more relevant for analysis / symbolic evaluation
-  --------------------------------------------------------------
+  | Predicate  [Name] Expr (Maybe OriginalRuleRef) -- Differs from a function when doing symbolic evaluation. Exact way in which they should differ is WIP.
+  | PredApp    Expr [Expr]
+
+  {--------------------------
+    Sigs and Relations
+  ---------------------------
   An Alloy Sig is a set
   Had added  sigs and relations
   because they're useful for certain sorts of analyses;
   but not sure that we really want them
   -}
-  | Predicate  [Name] Expr (Maybe OriginalRuleRef) -- Differs from a function when doing symbolic evaluation. Exact way in which they should differ is WIP.
-  | PredApp    Expr [Expr]
   | Sig        [Name] [Expr]                       -- Sig parents relations
   | Join       Expr Expr                           -- Relational join
   | Relation   Name Name Relatum (Maybe Text)      -- Relation relName relParentSigName relatum description
@@ -83,16 +86,16 @@ data Lit
   {- | ------------------------------------------------
   General background / context for the normative stuff
   -----------------------------------------------------
-  One way of adding normative stuff to a functional expression language amounts to augmenting the evaluator 
-  with a Store / EvalState; and in particular, keeping track of the statuses of the normative stuff 
-  (e.g. what obligations are operative at any point; what obligations have been satisfied or violated; what actions have been taken), 
-  as well as whatever other state is required. 
+  One way of adding normative stuff to a functional expression language amounts to augmenting the evaluator
+  with a Store / EvalState; and in particular, keeping track of the statuses of the normative stuff
+  (e.g. what obligations are operative at any point; what obligations have been satisfied or violated; what actions have been taken),
+  as well as whatever other state is required.
 
   It is for this reason that I'm modelling normative stuff as *statements* instead of *expressions*.
   Thinking of them as statements also agrees,
   not just with work in AI and AI x Law,
-  but also with a longstanding tradition in philosophy and linguistics 
-  that models them as being fundamentally *non*-truth-conditional, stateful things 
+  but also with a longstanding tradition in philosophy and linguistics
+  that models them as being fundamentally *non*-truth-conditional, stateful things
   (e.g., as 'proposals to update the conversational scoreboard').
 
   -----------
@@ -100,16 +103,16 @@ data Lit
   -----------
   Actions on this framework are basically impure functions. When an action is taken, the Store/EvalState is mutated in the necessary ways.
 
-  Can think of actions also as 'events'. 
-  This is perhaps clearest in the case of an 'atomic' action where there are no statements in its body. 
+  Can think of actions also as 'events'.
+  This is perhaps clearest in the case of an 'atomic' action where there are no statements in its body.
   Such atomic actions would be very similar to events in the SLEEC language.
 
   If you prefer to think in terms of transitions, this quote from John Camilleri may be helpful:
-    > An action is simply a transition which can only be taken when the corresponding action ... has been performed [by the agent in question]. 
+    > An action is simply a transition which can only be taken when the corresponding action ... has been performed [by the agent in question].
     > Each action also gets a corresponding *doer* automaton which sets the status of that action to *done*
 
   Misc:
-  * Currently distinguishing between Actions and Functions in the surface syntax, 
+  * Currently distinguishing between Actions and Functions in the surface syntax,
     because I think that having different syntax for pure vs impure functions will be helpful for end users. But not sure.
 -}
 data Statement
@@ -159,7 +162,8 @@ data Statement
       @
 -}
 data Deontic =
-  MkDeontic { name         :: Name -- Identifier for the deontic; may not need this given that Actions in effect are a way to name statements
+  MkDeontic { name         :: Maybe Name
+              -- Users can supply a name for the deontic if it doesn't appear in the scope of another Statement that already has a Name
             , agent        :: Name -- In the future may want to be able to quantify over agents too
             , deonticModal :: DeonticModal
             , action       :: Statement
@@ -170,5 +174,6 @@ data Deontic =
 data WIP_NotSureYet
   deriving stock (Eq, Show, Ord)
 
+-- | Can add prohibitions as sugar in the future
 data DeonticModal = Obligation | Permission
   deriving stock (Eq, Show, Ord)
