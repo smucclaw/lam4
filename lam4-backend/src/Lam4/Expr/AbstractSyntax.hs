9e2bb09d5e744e0fc31a9f6c73a8d5ce5f28f33b
ym-han <hanyongming@gmail.com>
2024-09-05 21:46:13 +0800

    Abstract Syntax: remove unnecessary type param

--- lam4-backend/src/Lam4/Expr/AbstractSyntax.hs
+++ lam4-backend/src/Lam4/Expr/AbstractSyntax.hs
@@ -8,19 +8,19 @@ import           Lam4.Expr.CommonSyntax
 import           Lam4.Expr.ConcreteSyntax qualified as CST (Lit (..), Decl)
 
 
-mkCEvalExpr :: ExprF CEvalExpr CST.Lit TypeExpr CST.Decl -> CEvalExpr
+mkCEvalExpr :: ExprF CEvalExpr CST.Lit CST.Decl -> CEvalExpr
 mkCEvalExpr = coerce
 
 -- | For wiring up to a non-symbolic evaluator like Simala
-newtype CEvalExpr = CEvalExpr (ExprF CEvalExpr CST.Lit TypeExpr CST.Decl)
+newtype CEvalExpr = CEvalExpr (ExprF CEvalExpr CST.Lit CST.Decl)
   deriving newtype (Eq, Show)
   deriving stock (Generic)
   
 -- | For symbolic evaluation
-newtype SymExpr = MkSymExpr (ExprF (Union SymExpr) SymLit TypeExpr Decl)
+newtype SymExpr = MkSymExpr (ExprF (Union SymExpr) SymLit Decl)
   deriving newtype (Eq, Show)
   deriving stock (Generic)
-  deriving (Mergeable, ExtractSym, EvalSym) via (Default (ExprF (Union SymExpr) SymLit TypeExpr Decl))
+  deriving (Mergeable, ExtractSym, EvalSym) via (Default (ExprF (Union SymExpr) SymLit Decl))
 
 
 newtype Decl = MyDecl (DeclF SymExpr TypeDecl)
@@ -29,7 +29,7 @@ newtype Decl = MyDecl (DeclF SymExpr TypeDecl)
   deriving (Mergeable, ExtractSym, EvalSym) via (Default (DeclF SymExpr TypeDecl))
 
 {- | Sep 2024: Normative constructs won't be added to AST till have wired to a basic evaluator -}
-data ExprF a lit typeE decl
+data ExprF a lit decl
   = Var        Name
   | Lit        lit
   | Cons       a a
@@ -53,7 +53,7 @@ data ExprF a lit typeE decl
   | Predicate  [Name] a (Maybe OriginalRuleRef) -- Differs from a function when doing symbolic evaluation. Exact way in which they should differ is WIP.
   | PredApp    a [a]
   | Sig        [Name] [a]                       -- Sig parents relations
-  | Relation   Name Name typeE (Maybe Text)     -- Relation relName relParentSigName relatum description
+  | Relation   Name Name TypeExpr (Maybe Text)     -- Relation relName relParentSigName relatum description
   deriving stock (Eq, Show, Generic)
 
 data SymLit
