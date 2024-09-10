2ece61581e2416ab3ed9e6e4e27bb16c1c4f97b0
ym-han <hanyongming@gmail.com>
2024-09-10 14:03:22 +0800

    lam4-cli: Add TODOs; comment out stuff that's tbd

--- lam4-cli/app/Main.hs
+++ lam4-cli/app/Main.hs
@@ -2,7 +2,7 @@ module Main where
 
 import           Base
 import qualified Base.ByteString             as BL
-import qualified Builder
+-- import qualified Builder
 import qualified Lam4.Expr.ConcreteSyntax    as CST (Decl)
 import           Lam4.Expr.Parser            (parseProgramByteStr)
 import           Lam4.Expr.ToConcreteEvalAST (cstProgramToConEvalProgram)
@@ -12,9 +12,9 @@ import           Lam4.Parser.Monad           (evalParserFromScratch)
 import           Options.Applicative
 
 data Command
-  = Build
-  | Compile [FilePath]
-  | Evaluate [FilePath]
+  = Compile [FilePath]
+  -- | Build
+  -- | Evaluate [FilePath]
   deriving (Show, Eq)
 
 commandParser :: Parser Command
@@ -23,25 +23,27 @@ commandParser =
     (command
        "compile"
        (info
+          -- TODO: Have the compilation start from Lam4 programs
           (Compile <$> many (strArgument (metavar "CONCRETE SYNTAX JSONs...")))
           (progDesc "Compile Lam4 programs to Simala"))
   -- <> command "build" (info (pure Build) (progDesc "Build the Lam4 backend"))
-       <> command
-            "eval"
-            (info
-               (Evaluate
-                  <$> many (strArgument (metavar "CONCRETE SYNTAX JSONs...")))
-               (progDesc "Evaluate Lam4 programs")))
+  --  <> command
+  --       "eval"
+  --       (info
+  --          (Evaluate
+  --             <$> many (strArgument (metavar "CONCRETE SYNTAX JSONs...")))
+  --          (progDesc "Evaluate Lam4 programs"))
+    )
 
 main :: IO ()
 main = do
   cmd <- execParser (info (commandParser <**> helper) fullDesc)
   case cmd of
-    Build -> undefined
     Compile files -> do
       smDecls <- parseAndCompile files
       pPrint $ ToSimala.render smDecls
-    Evaluate files -> undefined
+    -- Build -> undefined
+    -- Evaluate files -> undefined
       -- TODO: Add entrypoint eval
 
 parseAndCompile :: [FilePath] -> IO SimalaProgram
