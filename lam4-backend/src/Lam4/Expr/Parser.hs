9f7b33fc60b58f6c6e63c39f611cdd75c076981b
ym-han <hanyongming@gmail.com>
2024-09-23 00:34:43 +0800

    Parser.hs: Add comment with TODO

--- lam4-backend/src/Lam4/Expr/Parser.hs
+++ lam4-backend/src/Lam4/Expr/Parser.hs
@@ -61,6 +61,8 @@ import           Lam4.Parser.Type
 -- | The Langium-parser ASTNode `$type`s that correspond to recursive exprs
 recursiveTypes :: Set Text
 recursiveTypes = Set.fromList ["LetExpr", "FunDecl", "PredicateDecl"]
+-- TODO: Check how to handle Let vs Let rec in translation to Simala!
+
 
 {----------------------
     Ref
