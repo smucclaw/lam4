65901120b69af3f2002fddab6908bb01ed965840
ym-han <hanyongming@gmail.com>
2024-10-23 03:32:57 +0800

    Init infra for contract automata

---
+++ experiments/contract-automata/src/Automata.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE RecordWildCards #-}
+
+{-
+The key definitions and operations here are copied / adapted from this blog post:
+https://iagoleal.com/posts/automata-monads/
+-}
+
+module Automata where
+
+import Data.Foldable          (foldlM)
+import Control.Monad.Identity (Identity(..))
+import Data.Bifunctor         (second)
+import Data.Complex           (Complex, magnitude)
+import Data.Map qualified as Map
+
+
+-- | Finite automaton with state `s`, alphabet `a` and a monadic context `m`.
+--   The type parameters `s` and `a` are assumed to represent finite sets.
+data Automaton m s a = Automaton
+  { initial    :: s               -- ^ Initial State
+  , transition :: s -> a -> m s   -- ^ Change state with a context.
+  , accepting  :: s -> Bool       -- ^ Accepting subset as a predicate.
+  }
+
+run :: Monad m => Automaton m s a -> [a] -> m s
+run Automaton{..} = foldlM transition initial
+
+recognize :: (Finite s, Monad m, Context m) => Automaton m s a -> [a] -> Bool
+recognize aut@Automaton{..} = possible accepting . run aut
+
+class Context m where
+  possible :: Finite s => (s -> Bool) -> (m s -> Bool)
+
+class (Eq a, Ord a) => Finite a
+
+-- | Deterministic Finite Automaton.
+type DFA  = Automaton Identity
+
+instance Context Identity where
+  possible pred (Identity s) = pred s
