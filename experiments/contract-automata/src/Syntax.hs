dee8f6bb1d361da9f6b323f9b0234bb627236e81
ym-han <hanyongming@gmail.com>
2024-10-29 19:32:11 +0800

    CA exps: Simplify -- remove Sequence for now

--- experiments/contract-automata/src/Syntax.hs
+++ experiments/contract-automata/src/Syntax.hs
@@ -3,7 +3,14 @@ module Syntax where
 import Data.Text as T
 import Data.List.NonEmpty
 
-{-----------
+{-
+----------
+  TODOs
+----------
+  * Add !a
+  * Figure out where exactly to add Sequence. Not clear to me rn if it's best understood as an operator on clauses or on events or ...
+
+-------------
   Resources
 -------------
 
@@ -35,44 +42,48 @@ newtype Contract = MkContract { clauses :: NonEmpty Clause }
 
      Conjunction has been factored out to Contract -- a conjunction of clauses should be represented as a sequence of them (wrapped in a Contract).
 -}
-data Clause = Must   Party ActionForNorm   -- ^ Obligation
-            | May    Party ActionForNorm   -- ^ Permission
+data Clause = Must   Event   -- ^ Obligation
+            | May    Event   -- ^ Permission
             -- TODO: Think about whether to add a construct tt allows user to specify that these are the *only* options available to the actor
-            | Shant  Party ActionForNorm   -- ^ Prohibition
+            | Shant  Event   -- ^ Prohibition
             | If     Guard  Clause         -- ^ [β]C -- the contract C must be executed if action β is performed
             | Top                          -- ^ ⊤
             | Bottom                       -- ^ ⊥
   deriving (Eq, Ord, Show)
-
 
 -----------------------
 ---  Actions
 -----------------------
 
 -- | The notion of actions that's used with norms. In CL_rest, modalities (O, P and F) are only applied to atomic actions (CA p.28)
-type ActionForNorm = AtomicAction
+type ActionForNorm = Action
 
-{- | An AtomicAction, as I'm using it, does *not* include the agent / party.
+{- | An Action, as I'm using it, does *not* include the agent / party.
 Note that I differ from Camilieri's SCL on this: he uses "the term action to mean both agent and act together".
--}
-type AtomicAction = Text
 
-type Guard = CompoundAction
+β := 0 | 1 | a | !a | β&β |β.β |β∗
 
-{- | β := 0 | 1 | a | !a | β&β |β.β |β∗
   I omit the concurrency operator @&@ because
     (i) truly simultaneous actions are rare in the legal / regulation context
     (ii) it can be simulated with interleaving
     (iii) according to "A framework for conflict analysis of normative texts written
     in controlled natural language", it results in exponential blowup
   
-  TODO: Add !a
+  Useful note re Impossible and Skip, from "A framework for conflict analysis...":
+    We omit from the CNL the two special actions 0 and 1 since they have no obvious equivalent in English. Although they have useful algebraic properties in the logic, they do not appear naturally in any real contracts. 
+    A notable exception is the construction [1∗]C which means that the clause C must be enforced at any state. 
+    For this purpose, we added the keyword `always` which can be used in front of any clause, which adds the condition [1∗] in the corresponding CL formula. 
+    Similarly we did not include the Kleene star in our CNL, except for its use in relation to always.
 -}
-data CompoundAction =  AtomicAction AtomicAction                  -- ^ a
-                    | Sequence     CompoundAction CompoundAction
-                    -- | Impossible                                  -- ^ the impossible action, aka 0
-                    -- | Skip                                        -- ^ skip, aka 1: matches any action
-  deriving (Eq, Ord, Show)
+type Action = Text
+
+data Guard = GDone Event
+             -- ^ Party did Action. This corresponds to the `InfixOrPostfixActionApplication` construct in the Lam4 Langium grammar, and to Camilieri's `GDone Action`
+           | GNot Guard
+             -- ^ Negation of guard condition
+           | GTrue 
+           | GFalse
+  deriving stock (Eq, Ord, Show)
 
 
 -----------------------
@@ -84,7 +95,8 @@ newtype Trace = MkTrace { getTrace :: [Event] }
   deriving newtype (Eq, Ord)
   deriving stock Show
 
-{- |
+{- | Event aka 'state of affairs'.
+
 "We assume that
   * each event is in fact a pair containing the event itself and who is the actor (subject, or performer of the event),
 
