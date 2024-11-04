8e7da45b8d7f58c685dd324843d688457797af77
ym-han <hanyongming@gmail.com>
2024-11-04 16:24:56 +0800

    Remove things that aren't currently being used

--- experiments/contract-automata/src/Automata.hs
+++ experiments/contract-automata/src/Automata.hs
@@ -38,11 +38,3 @@ type DFA = Automaton Identity
 
 instance Context Identity where
   possible pred (Identity s) = pred s
-
-
--- -- | Non-deterministic Finite Automaton.
--- type NFA = Automaton []
-
--- instance Context [] where
---  possible :: Finite s => (s -> Bool) -> [s] -> Bool
---  possible = any
