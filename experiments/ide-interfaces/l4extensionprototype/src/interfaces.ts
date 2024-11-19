268e6c03fb003063f336810783cc6ddd6d818f7d
ym-han <hanyongming@gmail.com>
2024-11-19 19:21:47 +0800

    Add initial draft of `VisualizeProgramRequest` (it should be fairly clear what the json schema and haskell versions should be, but we can also automatically generate JSON schema from the Zod schema)

---
+++ experiments/ide-interfaces/l4extensionprototype/src/interfaces.ts
@@ -0,0 +1,86 @@
+import { Uri } from "vscode";
+import { RequestType } from "vscode-languageclient/node";
+import { z } from "zod";
+
+/*******************************
+     VisualizeProgramRequest
+********************************/
+
+/**
+ * Request type for visualizing L4 programs.
+ * Can think in the future about whether to have different requests for visualizations of different L4 constructs.
+ *
+ * @type {RequestType<VisualizeProgramInfo, AsyncResult<VisualizeProgramResponse>, any>}
+ *
+ * @remarks
+ * What RequestType's type parameters mean (yes, `RequestType` is a confusing type):
+ * - Params: VisualizeProgramInfo
+ * - Result: AsyncResult<VisualizeProgramResponse, VisualizeProgramError>
+ * - Error: any (I don't currently understand what this type parameter does, but it shouldn't matter for our purposes.)
+ */
+export namespace VisualizeProgramRequest {
+  export const type: RequestType<
+    VisualizeProgramInfo,
+    AsyncResult<VisualizeProgramResponse, VisualizeProgramError>,
+    any
+  > = new RequestType("l4/visualizeProgram");
+}
+
+/** Quicky hacky type guard */
+const isUri = (val: unknown): val is Uri => {
+  return val !== null && typeof val === "object" && "toString" in val && "scheme" in val && "path" in val;
+};
+
+const uriSchema = z
+  .string()
+  .transform((str) => Uri.parse(str))
+  .or(z.custom<Uri>(isUri))
+  .transform((uri) => uri.toString());
+
+export const visualizeProgramInfoSchema = z.object({
+  program: uriSchema,
+});
+
+export const visualizeProgramResponseSchema = z.object({
+  html: z.string(),
+});
+
+export const visualizeProgramErrorSchema = z.object({
+  errorMessage: z.string(),
+});
+
+export type VisualizeProgramInfo = z.infer<typeof visualizeProgramInfoSchema>;
+export type VisualizeProgramResponse = z.infer<typeof visualizeProgramResponseSchema>;
+export type VisualizeProgramError = z.infer<typeof visualizeProgramErrorSchema>;
+
+/******************
+     Util types
+*******************/
+
+// From https://github.com/gvergnaud/ts-pattern/blob/b5aa458c031c08150689be8c4a2d0dc0c2b10093/tests/types-catalog/utils.ts#L78
+
+type AsyncResultStatus = "idle" | "loading" | "error" | "success";
+
+export interface BaseAsyncResult<TData, TError = Error> {
+  status: AsyncResultStatus;
+  data?: TData;
+  error?: TError;
+}
+
+export interface AsyncResultIdleOrLoading<TData, TError = Error> extends BaseAsyncResult<TData, TError> {
+  status: "idle" | "loading";
+}
+
+export interface AsyncResultSuccess<TData, TError = Error> extends BaseAsyncResult<TData, TError> {
+  status: "success";
+  data: TData;
+}
+
+export interface AsyncResultError<TData, TError = Error> extends BaseAsyncResult<TData, TError> {
+  status: "error";
+  error: TError;
+}
+export type AsyncResult<TData, TError = Error> =
+  | AsyncResultIdleOrLoading<TData, TError>
+  | AsyncResultSuccess<TData, TError>
+  | AsyncResultError<TData, TError>;
